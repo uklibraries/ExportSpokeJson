cdf373235f44aba56d5b8f0d1c516231da5735a1
Michael Slone <m.slone@gmail.com>
2015-03-26 10:20:19 -0400

    add series and interview metadata

--- models/Output/SpokeJson.php
+++ models/Output/SpokeJson.php
@@ -17,10 +17,10 @@ class Output_SpokeJson
             $metadata = $this->getCollectionFields($item);
             break;
         case "series":
-            $metadata = array('foo' => 'series');
+            $metadata = $this->getSeriesFields($item);
             break;
         case "interviews":
-            $metadata = array('foo' => 'interviews');
+            $metadata = $this->getInterviewFields($item);
             break;
         }
         $this->_metadata = $metadata;
@@ -43,6 +43,59 @@ class Output_SpokeJson
             'CollectionDIP_display' => metadata($item, array('Item Type Metadata', 'Collection DIP')),
             'CollectionSummary_display' => metadata($item, array('Item Type Metadata', 'Collection Summary')),
             'CollectionThemeR_display' => metadata($item, array('Item Type Metadata', 'Collection Theme')),
+        );
+
+        return $metadata;
+    }
+
+    public function getSeriesFields($item)
+    {
+        $metadata = array(
+            'id' => metadata($item, array('Dublin Core', 'Identifier')),
+            'rights_display' => metadata($item, array('Dublin Core', 'Rights')),
+            'format_display' => metadata($item, array('Dublin Core', 'Format')),
+            'SeriesExternalLinkR_display' => metadata($item, array('Item Type Metadata', 'Series External Link')),
+            'SeriesInterviewR_display' => metadata($item, array('Item Type Metadata', 'Series Interview')),
+            'SeriesNumberofInterviews_display' => metadata($item, array('Item Type Metadata', 'Series Number of Interviews')),
+            'SeriesTitle_display' => metadata($item, array('Item Type Metadata', 'Series Title')),
+            'SeriesLCSubjectR_display' => metadata($item, array('Item Type Metadata', 'Series LC Subject')),
+            'SeriesKeywordR_display' => metadata($item, array('Item Type Metadata', 'Series Keyword')),
+            'SeriesThemeR_display' => metadata($item, array('Item Type Metadata', 'Series Theme')),
+            'SeriesAccession_display' => metadata($item, array('Item Type Metadata', 'Series Accession')),
+            'SeriesCollection_display' => metadata($item, array('Item Type Metadata', 'Series Collection')),
+            'SeriesSummary_display' => metadata($item, array('Item Type Metadata', 'Series Summary')),
+            'SeriesMasterType_display' => metadata($item, array('Item Type Metadata', 'Series Master Type')),
+        );
+
+        return $metadata;
+    }
+
+    public function getInterviewFields($item)
+    {
+        $metadata = array(
+            'id' => metadata($item, array('Dublin Core', 'Identifier')),
+            'rights_display' => metadata($item, array('Dublin Core', 'Rights')),
+            'format_display' => metadata($item, array('Dublin Core', 'Format')),
+            'InterviewUsageStatement_display' => metadata($item, array('Item Type Metadata', 'Interview Usage Statement')),
+            'InterviewExternalLinkR_display' => metadata($item, array('Item Type Metadata', 'Interview External Link')),
+            'InterviewYear_display' => metadata($item, array('Item Type Metadata', 'Interview Year')),
+            'InterviewRestrictionDetails_display' => metadata($item, array('Item Type Metadata', 'Interview Restriction Details')),
+            'InterviewMonth_display' => metadata($item, array('Item Type Metadata', 'Interview Month')),
+            'InterviewCacheFile_display' => metadata($item, array('Item Type Metadata', 'Interview Cache File')),
+            'InterviewKeywordsR_display' => metadata($item, array('Item Type Metadata', 'Interview Keywords')),
+            'InterviewDate_display' => metadata($item, array('Item Type Metadata', 'Interview Date')),
+            'InterviewerR_display' => metadata($item, array('Item Type Metadata', 'Interviewer')),
+            'IntervieweeR_display' => metadata($item, array('Item Type Metadata', 'Interviewee')),
+            'InterviewAccessionNumber_display' => metadata($item, array('Item Type Metadata', 'Interview Accession Number')),
+            'InterviewTitle_display' => metadata($item, array('Item Type Metadata', 'Interview Title')),
+            'InterviewDay_display' => metadata($item, array('Item Type Metadata', 'Interview Day')),
+            'UseRestrictions_display' => metadata($item, array('Item Type Metadata', 'Use Restrictions')),
+            'OnlineIdentifier_display' => metadata($item, array('Item Type Metadata', 'Online Identifier')),
+            'InterviewMasterType_display' => metadata($item, array('Item Type Metadata', 'Interview Master Type')),
+            'InterviewRightsStatement_display' => metadata($item, array('Item Type Metadata', 'Interview Rights Statement')),
+            'InterviewLCSubjectR_display' => metadata($item, array('Item Type Metadata', 'Interview LC Subject')),
+            'InterviewCollection_display' => metadata($item, array('Item Type Metadata', 'Interview Collection')),
+            'InterviewSeries_display' => metadata($item, array('Item Type Metadata', 'Interview Series')),
         );
 
         return $metadata;
