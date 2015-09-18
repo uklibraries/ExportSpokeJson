6682bdac0193bed7adce265b2ed20a88c690594c
Michael Slone <m.slone@gmail.com>
2015-09-18 09:34:58 -0400

    update export suppression model

--- jobs/ExportSpokeJson_Job_ExportItem.php
+++ jobs/ExportSpokeJson_Job_ExportItem.php
@@ -13,44 +13,47 @@ class ExportSpokeJson_Job_ExportItem extends Omeka_Job_AbstractJob
         $path = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'tmp';
         $item = get_record_by_id('Item', $this->_options['itemId']);
         $output = new Output_SpokeJson($item);
-        $filename = $path . DIRECTORY_SEPARATOR . $output->id() . '.json';
-        file_put_contents($filename, $output->toJSON());
-        chmod($filename, fileperms($filename) | 16);
-        switch ($output->itemType()) {
-        case "collections":
-            $objects = get_db()->getTable('ItemRelationsRelation')->findByObjectItemId($item->id);
-            $objectRelations = array();
-            foreach ($objects as $object) {
-                if ($object->getPropertyText() !== "Is Part Of") {
-                    continue;
+
+        if ($output->exportable()) {
+            $filename = $path . DIRECTORY_SEPARATOR . $output->id() . '.json';
+            file_put_contents($filename, $output->toJSON());
+            chmod($filename, fileperms($filename) | 16);
+            switch ($output->itemType()) {
+            case "collections":
+                $objects = get_db()->getTable('ItemRelationsRelation')->findByObjectItemId($item->id);
+                $objectRelations = array();
+                foreach ($objects as $object) {
+                    if ($object->getPropertyText() !== "Is Part Of") {
+                        continue;
+                    }
+                    if (!($subitem = get_record_by_id('item', $object->subject_item_id))) {
+                        continue;
+                    }
+                    Zend_Registry::get('bootstrap')->getResource('jobs')->sendLongRunning(
+                        'ExportSpokeJson_Job_ExportItem', array(
+                            'itemId' => $object->subject_item_id,
+                        )
+                    );
                 }
-                if (!($subitem = get_record_by_id('item', $object->subject_item_id))) {
-                    continue;
+                break;
+            case "series":
+                $objects = get_db()->getTable('ItemRelationsRelation')->findByObjectItemId($item->id);
+                $objectRelations = array();
+                foreach ($objects as $object) {
+                    if ($object->getPropertyText() !== "Is Part Of") {
+                        continue;
+                    }
+                    if (!($subitem = get_record_by_id('item', $object->subject_item_id))) {
+                        continue;
+                    }
+                    Zend_Registry::get('bootstrap')->getResource('jobs')->sendLongRunning(
+                        'ExportSpokeJson_Job_ExportItem', array(
+                            'itemId' => $object->subject_item_id,
+                        )
+                    );
                 }
-                Zend_Registry::get('bootstrap')->getResource('jobs')->sendLongRunning(
-                    'ExportSpokeJson_Job_ExportItem', array(
-                        'itemId' => $object->subject_item_id,
-                    )
-                );
+                break;
             }
-            break;
-        case "series":
-            $objects = get_db()->getTable('ItemRelationsRelation')->findByObjectItemId($item->id);
-            $objectRelations = array();
-            foreach ($objects as $object) {
-                if ($object->getPropertyText() !== "Is Part Of") {
-                    continue;
-                }
-                if (!($subitem = get_record_by_id('item', $object->subject_item_id))) {
-                    continue;
-                }
-                Zend_Registry::get('bootstrap')->getResource('jobs')->sendLongRunning(
-                    'ExportSpokeJson_Job_ExportItem', array(
-                        'itemId' => $object->subject_item_id,
-                    )
-                );
-            }
-            break;
         }
     }
 }
